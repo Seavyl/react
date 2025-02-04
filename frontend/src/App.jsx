39f3d7f9c8e993d5e13cc2dac953af16c2e668a4
lombardi <charlene.lombardi@ik.me>
2025-02-04 10:58:49 +0100

    milieu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 
 import './App.css'
+import Home from "./pages/Home"
 
 function App() {
-  
+  return(
+    <>
+        <Home />
+    </>
+  )
 }
   
   
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,12 +6,25 @@ function Home(){
         {id: 2, title: "Terminator", release_date:"1995"},
         {id: 3, title: "Matrix", release_date:"1998"},
         
-    ] 
+    ];
 
-    return <div className ="home">
+    const handleSearch = () =>{};
+
+    return (
+    <div className ="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input type="text" placeholder="Search for movies..." className="serach-input"></input>
+        </form>
+
         <div className="movies-grid">
-            {movies.map(movie => <MovieCard />)}
+            {movies.map((movie) => (
+                <MovieCard movie={movie} key={movie.id} />
+                ))}
         </div>
 
     </div>
-}
+    );
+
+    }
+
+export default Home
