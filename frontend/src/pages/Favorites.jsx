4b4f7719dfe626bbf3aa23591e9ed5ef54ba131d
lombardi <charlene.lombardi@ik.me>
2025-03-10 16:13:29 +0100

    css réussi

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,9 +1,11 @@
 import "../css/Favorites.css"
 function Favorites(){
-    return <div className="favourites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p> start adding movies to your favorites and they will appear here</p>
-    </div>
+    return (
+        <div className="favorites-empty">
+          <h2>No Favorite Movies Yet</h2>
+          <p>Start adding movies to your favorites and they will appear here!</p>
+        </div>
+      );
 }
 
 export default Favorites
